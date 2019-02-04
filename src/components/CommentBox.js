ba00bc9dd24b87325eaf8791da72e023aff67b26
Pavel Gric <pavel@ideatech.cz>
2019-02-04 14:04:28 +0100

    Initial setup

---
+++ src/components/CommentBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div>
+        <p>CommentBox</p>
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
---
+++ src/components/CommentList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentList extends Component {
+  render() {
+    return (
+      <div>
+        <p>CommentList</p>
+      </div>
+    );
+  }
+}
+
+export default CommentList;
