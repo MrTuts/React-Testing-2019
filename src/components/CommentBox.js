ed9ff925ad4b477862dd26172ccbc59a33b683cc
Pavel Gric <pavel@ideatech.cz>
2019-02-04 18:33:31 +0100

    Test CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = {
+    comment: '',
+  };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ comment: '' });
+  };
+
   render() {
     return (
-      <div>
-        <p>CommentBox</p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
     );
   }
 }
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import CommentBox from 'components/CommentBox';
+
+describe('<CommentBox />', () => {
+  let wrapped;
+
+  beforeEach(() => {
+    wrapped = mount(<CommentBox />);
+  });
+
+  afterEach(() => {
+    wrapped.unmount();
+  });
+
+  it('has textarea and button', () => {
+    expect(wrapped.find('textarea').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(1);
+  });
+
+  describe('the textarea', () => {
+    beforeEach(() => {
+      wrapped
+        .find('textarea') //find text area
+        .simulate('change', { target: { value: 'new comment' } }); //simulate change
+
+      // setState is async, force re-render to be sure it happened
+      wrapped.update();
+    });
+
+    it('has textarea that user can type in', () => {
+      expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    });
+
+    it('should reset textarea when form is submitted', () => {
+      wrapped.find('form').simulate('submit', {});
+      wrapped.update();
+
+      expect(wrapped.find('textarea').prop('value')).toEqual('');
+    });
+  });
+});
